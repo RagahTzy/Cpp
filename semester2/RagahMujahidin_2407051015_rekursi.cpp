ae77ca9fce8bd7b9d39480e9ee803fd23d72be42
RagahTzy <ragahediting@gmail.com>
2025-05-14 21:26:23 +0700

    minor change pada rekursi teori

--- semester2/RagahMujahidin_2407051015_rekursi.cpp
+++ semester2/RagahMujahidin_2407051015_rekursi.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int rekursi(string random, char huruf, int index = 0) {
     if (index >= random.length()) return 0;
+    /*  pakai random.length() untuk menghitung panjang string
+        agar dapat mengetahui posisi index apakah sudah mencapai
+        akhir string atau belum.
+        kalo udah di akhir string, return 0
+    */
     if (random[index] == huruf) {
         return 1 + rekursi(random, huruf, index + 1);
     } else {
