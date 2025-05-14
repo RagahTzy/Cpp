a1a80009b2958da322b6dd0aecc6b3f548a438ba
RagahTzy <ragahediting@gmail.com>
2025-05-14 21:20:06 +0700

    perubahan pada rekursi teori

--- semester2/RagahMujahidin_2407051015_rekursi.cpp
+++ semester2/RagahMujahidin_2407051015_rekursi.cpp
@@ -14,10 +14,16 @@ int rekursi(string random, char huruf, int index = 0) {
 int main() {
     string random;
     char huruf;
-    cout << "Masukkan string: ";
+    cout << "Masukkan string (maksimal 100 huruf): ";
     getline(cin, random);
+    if (random.length() > 100) {
+        cout << "Error: String tidak boleh lebih dari 100 huruf." << endl;
+        return 1;
+    }
     cout << "Masukkan huruf yang dicari: ";
     cin >> huruf;
     int jumlah = rekursi(random, huruf);
     cout << "Jumlah huruf '" << huruf << "' dalam string: " << jumlah << endl;
+
+    return 0;
 }
