b9d83bdaa20030a6c8d5379a85391308bfb6de16
RagahTzy <ragahediting@gmail.com>
2025-05-19 12:41:51 +0700

    update tugas class teori

--- semester2/RagahMujahidin_2407051015_Class.cpp
+++ semester2/RagahMujahidin_2407051015_Class.cpp
@@ -8,11 +8,11 @@ private:
     string nama;
     string jurusan;
 public:
-    //Setter
+    //setter
     void setNim(long long n) { nim = n; }
     void setNama(string nm) { nama = nm; }
     void setJurusan(string jur) { jurusan = jur; }
-    //Getter
+    //getter
     long long getNim() const { return nim; }
     string getNama() const { return nama; }
     string getJurusan() const { return jurusan; }
@@ -27,13 +27,47 @@ int main(){
         Mahasiswa mhs;
         long long nim;
         string nama, jurusan;
-        cout << "Masukkan NIM (12 digit): ";
-        cin >> nim;
+        //pas masukin nim, nama, dan jurusan dibikin looping
+        //supaya kalo input lebih dari panjang yang ditentukan
+        //dia bakal disuruh input ulang, sampe memenuhi panjang yang ditentukan
+        while (true) {
+            cout << "Masukkan NIM (maks 12 digit): ";
+            cin >> nim;
+            //hitung jumlah digit NIM
+            long long temp = nim;
+            int digit = 0;
+            if (temp == 0) digit = 1;
+            else {
+                while (temp != 0) {
+                    temp /= 10;
+                    digit++;
+                }
+            }
+            if (digit <= 12 && nim >= 0) {
+                break;
+            } else {
+                cout << "Error: NIM harus maksimal 12 digit dan tidak boleh negatif!\n";
+            }
+        }
         cin.ignore(); // agar getline bisa dipakai kembali
-        cout << "Masukkan Nama (maks 32 karakter): ";
-        getline(cin, nama);
-        cout << "Masukkan Jurusan (maks 32 karakter): ";
-        getline(cin, jurusan);
+        while (true) {
+            cout << "Masukkan Nama (maks 32 karakter): ";
+            getline(cin, nama);
+            if (nama.length() <= 32) {
+                break;
+            } else {
+                cout << "Error: Nama maksimal 32 karakter!\n";
+            }
+        }
+        while (true) {
+            cout << "Masukkan Jurusan (maks 32 karakter): ";
+            getline(cin, jurusan);
+            if (jurusan.length() <= 32) {
+                break;
+            } else {
+                cout << "Error: Jurusan maksimal 32 karakter!\n";
+            }
+        }
         mhs.setNim(nim);
         mhs.setNama(nama);
         mhs.setJurusan(jurusan);
